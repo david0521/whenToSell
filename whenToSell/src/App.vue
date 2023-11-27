d1110e12ce1f236e8dc4af225a845c5c19f51495
David Hong <davidbor@net.chalmers.se>
2023-11-28 00:48:01 +0100

    #1 Add top-header  The top header is now here and the anticipated menu is all here too.  However, the pages are not implemented yet so it is not possible to connect to the pages.

---
+++ whenToSell/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+      <NavBar/>
+  </header>
+  </template>
+
+  <script setup>
+  import NavBar from './components/NavBar.vue'
+  </script>
+  <style>
+  #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+  </style>
---
+++ whenToSell/src/components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="secondary">
+      <b-navbar-brand href="/">SEK</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/" style="color: white;">Home</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown style="color: white;" right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>Account</em>
+            </template>
+            <b-dropdown-item href="my-page">My Page</b-dropdown-item>
+            <b-dropdown-item href="login">Login</b-dropdown-item>
+            <b-dropdown-item href="sign-up">Sign Up</b-dropdown-item>
+            <b-dropdown-item href="logout">Logout</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+
+
